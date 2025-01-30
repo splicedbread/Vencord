dfcf68a46d1a6caada3aef7e25a37507e3b1ec98
splicedbread <jake.vr.84116@gmail.com>
2025-01-30 00:49:30 -0800

    Plugin Boilerplate

---
+++ src/plugins/invertedSidebar/index.ts
@@ -0,0 +1,14 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "invertedSidebar",
+    description: "Inverts the sidebar position & new DM pop location",
+    authors: [Devs.splicedbread],
+});
